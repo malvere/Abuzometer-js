9a13a89bb0bce0b777a494d1e58eb30fc2796034
Malvere <gatling1404@gmail.com>
2024-01-16 01:24:52 +0300

    tg

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { kApp, kPage, kNavbar, kBlock } from 'konsta/vue';
+</script>
+
+<template>
+  <k-app theme="ios">
+    <k-page>
+      <k-navbar title="My App" />
+      <k-block>
+        <p>Here comes my app</p>
+      </k-block>
+    </k-page>
+  </k-app>
+</template>

9e09479d35ee03d5ef11c5eb317af57c993f7b16
Malvere <gatling1404@gmail.com>
2024-01-19 00:25:49 +0300

    tg

--- src/components/CalcInputs.vue
+++ src/components/CalcInputs.vue
@@ -49,7 +49,7 @@ const openPopup = () => {
         :value="smmPrice"
         inputmode="numeric"
         @change="(e) => (smmPrice = e.target.value)"
-        @blur="() => (ri1 = true, ri1 = false)"
+        @blur="() => (ri1 = true)"
       >
         <template #media> <demo-icon /> </template>
       </k-list-input>

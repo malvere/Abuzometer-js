6c620418f08d308336fac4f07f0336d1e97885bf
Malvere <gatling1404@gmail.com>
2024-01-27 15:31:42 +0300

    tg

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <script setup>
-// import { kApp, kPage, kNavbar, kBlock } from 'konsta/vue';
 import { kApp, kPage } from 'konsta/vue'
 import { RouterView } from 'vue-router'
 import AuthProvider from './providers/AuthProvider.vue'
-// const pageTitle = 'Abuzomer';
+
 const hmackey = '35df66f89c4336512a805a656125d757842a2d01a1b341d7ab91a702cc5aa236'
 </script>
 
 <template>
   <k-app theme="ios">
     <k-page>
-      <component :is="hmackey ? AuthProvider : 'div'" :hex_hmac_signature="hmackey">
+      <AuthProvider :hex_hmac_signature="hmackey">
         <RouterView />
         <template #unauthorized v-if="hmackey"> Не карлик </template>
-      </component>
+      </AuthProvider>
     </k-page>
   </k-app>
 </template>
--- src/providers/AuthProvider.vue
+++ src/providers/AuthProvider.vue
@@ -1,72 +1,76 @@
 <script setup>
-    import { ref, provide } from 'vue';
-    import WebApp from '@twa-dev/sdk';
-    import hmac from 'js-crypto-hmac';
+import { ref, provide, onMounted } from 'vue'
+import WebApp from '@twa-dev/sdk'
+import hmac from 'js-crypto-hmac'
 
-    const props = defineProps({
-        hex_hmac_signature: {
-            type: String,
-            required: true,
-        }
-    });
+const props = defineProps({
+  hex_hmac_signature: {
+    type: String,
+    required: true
+  }
+})
 
-    const clientAuthorized = ref(null);
+const clientAuthorized = ref(null)
 
-    const ksort = (obj) => {
-        const sortedKeys = Object.keys(obj).sort();
-        const sortedObj = {};
+const ksort = (obj) => {
+  const sortedKeys = Object.keys(obj).sort()
+  const sortedObj = {}
 
-        sortedKeys.forEach(key => {
-            sortedObj[key] = obj[key];
-        });
+  sortedKeys.forEach((key) => {
+    sortedObj[key] = obj[key]
+  })
 
-        return sortedObj;
-    };
+  return sortedObj
+}
 
-    const buffer2Hex = (buffer) => {
-        return [...new Uint8Array(buffer)]
-        .map(x => x.toString(16).padStart(2, '0'))
-        .join('');
-    };
+const buffer2Hex = (buffer) => {
+  return [...new Uint8Array(buffer)].map((x) => x.toString(16).padStart(2, '0')).join('')
+}
 
-    const hex2Buffer = (hexString) => {
-        const bytes = [];
-        for (let i = 0; i < hexString.length; i += 2) {
-            bytes.push(parseInt(hexString.substr(i, 2), 16));
-        }
-        return new Uint8Array(bytes);
-    };
+const hex2Buffer = (hexString) => {
+  const bytes = []
+  for (let i = 0; i < hexString.length; i += 2) {
+    bytes.push(parseInt(hexString.substr(i, 2), 16))
+  }
+  return new Uint8Array(bytes)
+}
 
-    let initDataUnsafe = {...WebApp.initDataUnsafe};
-    let initDataHash   = initDataUnsafe.hash;
-    let initDataString = '';
+let initDataUnsafe = { ...WebApp.initDataUnsafe }
+let initDataHash = initDataUnsafe.hash
+let initDataString = ''
 
-    delete initDataUnsafe['hash'];
-    initDataUnsafe = ksort(initDataUnsafe);
+delete initDataUnsafe['hash']
+initDataUnsafe = ksort(initDataUnsafe)
 
-    initDataString = Object.entries(initDataUnsafe)
-    .map(([key, value]) => {
-        value = typeof value === 'object' ? JSON.stringify(value, null, 0) : value;
-        return `${key}=${value}`;
-    })
-    .join('\n');
+initDataString = Object.entries(initDataUnsafe)
+  .map(([key, value]) => {
+    value = typeof value === 'object' ? JSON.stringify(value, null, 0) : value
+    return `${key}=${value}`
+  })
+  .join('\n')
 
-    hmac.compute(hex2Buffer(props.hex_hmac_signature), new TextEncoder().encode(initDataString), 'SHA-256').then((result) => {
+const checkAuthorization = async () => {
+  try {
+    const result = await hmac.compute(
+      hex2Buffer(props.hex_hmac_signature),
+      new TextEncoder().encode(initDataString),
+      'SHA-256'
+    )
+    clientAuthorized.value = buffer2Hex(result) === initDataHash
+    provide('authorized', clientAuthorized.value)
+  } catch (error) {
+    console.error('Error checking authorization:', error)
+  }
+}
 
-        clientAuthorized.value = buffer2Hex(result) === initDataHash;
-
-    });
-
-    provide('authorized', clientAuthorized.value);
+onMounted(() => {
+  checkAuthorization()
+})
 </script>
 
 <template>
-    <slot v-if="clientAuthorized" />
-    <slot v-else name="unauthorized">
-        Unauthorized Telegram Client
-    </slot>
+  <slot v-if="clientAuthorized" />
+  <slot v-else name="unauthorized"> Unauthorized Telegram Client </slot>
 </template>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>

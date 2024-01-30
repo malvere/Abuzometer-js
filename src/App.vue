ff8da278f9f1749e027bbe5b9841204ec6636533
Malvere <gatling1404@gmail.com>
2024-01-30 22:59:39 +0300

    token support

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,45 @@ import { kApp, kPage } from 'konsta/vue'
 import { RouterView } from 'vue-router'
 import AuthProvider from './providers/AuthProvider.vue'
 import UnauthorizedView from './views/UnauthorizedView.vue'
-
-const hmackey = '35df66f89c4336512a805a656125d757842a2d01a1b341d7ab91a702cc5aa236'
+import WebApp from '@twa-dev/sdk'
+import { onMounted, ref } from 'vue'
+import {apiEndpoints} from '@/api'
+const state = ref({
+  hasResponse: false,
+  key: null
+})
+onMounted(async () => {
+  const uid = WebApp.initDataUnsafe.user.id
+  try {
+    const response = await fetch(apiEndpoints.key, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ user_id: uid }),
+    });
+    if (!response.ok) {
+      throw new Error(`Failed with ${response.status}`)
+    }
+    response.json().then((data) => state.value.key = data.key)
+  } catch (error) {
+    console.error('Error fetching HMAC key: ', error)
+  }
+})
 </script>
 
 <template>
   <k-app theme="ios">
-    <k-page>
-      <AuthProvider :hex_hmac_signature="hmackey">
+    <k-page v-if="state.hasResponse">
+      <AuthProvider :hex_hmac_signature="state.key">
         <RouterView />
-        <template #unauthorized v-if="hmackey">
+        <template #unauthorized v-if="state.key">
           <UnauthorizedView />
         </template>
       </AuthProvider>
+    </k-page>
+    <k-page v-else>
+      Loading...
     </k-page>
   </k-app>
 </template>

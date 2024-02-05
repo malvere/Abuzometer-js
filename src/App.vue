7cbae59226bfd30a679bb2df6ac5d5daf4883f82
Malvere <gatling1404@gmail.com>
2024-02-05 03:20:44 +0300

    auth update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { kApp, kPage } from 'konsta/vue'
+import { kApp, kPage, kBlockTitle, kList, kListInput, kListButton } from 'konsta/vue'
 import { RouterView } from 'vue-router'
 import AuthProvider from './providers/AuthProvider.vue'
 import UnauthorizedView from './views/UnauthorizedView.vue'
@@ -12,6 +12,7 @@ const state = ref({
   hasResponse: false,
   key: null
 })
+const inviteCode = ref('')
 
 onMounted(async () => {
   const uid = WebApp.initDataUnsafe.user.id
@@ -21,12 +22,44 @@ onMounted(async () => {
     })
     state.value.hasResponse = true
     state.value.key = response.data.key
-    console.log(response.data)
+    console.log(response.status)
   } catch (error) {
     console.error('Error fetching HMAC key: ', error)
   }
 })
+
+const authUser = async () => {
+  const uid = WebApp.initDataUnsafe.user.id
+  try {
+    const conf = {
+      params: {
+        tgid: uid,
+        code: inviteCode.value
+      }
+    }
+    const response = await axios.get(apiEndpoints.user, conf)
+    console.log('User Authenticated:', response.data);
+  } catch (error) {
+    console.error('Error creating user: ', error)
+  }
+
+}
+
 </script>
+
+<!-- <template>
+  <k-app theme="ios">
+    <k-page v-if="state.hasResponse">
+      <AuthProvider :hex_hmac_signature="state.key">
+        <RouterView />
+        <template #unauthorized v-if="state.key">
+          <UnauthorizedView />
+        </template>
+      </AuthProvider>
+    </k-page>
+    <k-page v-else> Loading... </k-page>
+  </k-app>
+</template> -->
 
 <template>
   <k-app theme="ios">
@@ -38,6 +71,18 @@ onMounted(async () => {
         </template>
       </AuthProvider>
     </k-page>
-    <k-page v-else> Loading... </k-page>
+    <k-page v-else> 
+      <k-block-title>Отказ в доступе</k-block-title>
+      <k-list inset strong>
+        <k-list-input
+        label="Инвайт"
+        type="text"
+        placeholder="Введите инвайт код"
+        :value="inviteCode"
+        @change="(e) => (inviteCode = e.target.value)"
+      />
+      <k-list-button class="k-color-brand-red" @click="authUser()"> Авиторизация </k-list-button>
+      </k-list>
+    </k-page>
   </k-app>
 </template>
--- src/api.js
+++ src/api.js
@@ -1,6 +1,8 @@
 //api.js
 const backendUrl = 'https://calc-udw5.onrender.com'
+// const backendUrl = 'http://localhost:8080'
 
 export const apiEndpoints = {
     key: `${backendUrl}/tg`,
+    user: `${backendUrl}/user`
 }

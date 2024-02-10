ed641b3713d4aaa1b82cf2d32f75c64c50554b3f
Malvere <vaderreg@gmail.com>
2024-02-10 04:11:51 +0300

    fixed api url

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 //api.js
-// const backendUrl = 'https://calc-udw5.onrender.com'
-const backendUrl = 'http://localhost:8080'
+const backendUrl = 'https://calc-udw5.onrender.com'
+// const backendUrl = 'http://localhost:8080'
 
 export const apiEndpoints = {
     key: `${backendUrl}/tg`,
